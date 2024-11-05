1b1768399d1e25f50d7259c8f84764be81097347
Awoodwhale <woodwhale@qq.com>
2024-11-05 14:54:22 +0800

    fix: fix crypto.js and add blur shortcode

--- static/js/crypto.js
+++ static/js/crypto.js
@@ -7,41 +7,47 @@ const aesDecrypt = (data, key) => {
     const decrypted = CryptoJS.AES.decrypt(data, cypherKey, cfg)
     return decrypted.toString(CryptoJS.enc.Utf8)
 }
-
 export const decryptContent = (hash) => {
     const pwd = document.getElementById(`pwd-${hash}`).value
     const data = document.getElementById(`data-${hash}`).value
     if (!pwd || !data) return
-    try {
-        loadJs('https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js').then(() => {
-            const res = aesDecrypt(data, pwd)
-            if (!res) {
-                alertify.error('密码错误')
-                document.getElementById(`pwd-${hash}`).value = ''
-                return
-            }
-            alertify.success('解密成功')
-            document.getElementById(`secret-content-${hash}`).remove()
-            loadJs('https://cdnjs.cloudflare.com/ajax/libs/marked/14.1.3/marked.min.js').then(() => {
-                const id = `decrypted-content-${hash}`
-                const contianer = document.getElementById(id)
-                contianer.innerHTML += marked.parse(res)
-                contianer.querySelectorAll("h1, h2, h3, h4, h5").forEach(h => {
-                    h.id = h.innerText.trim().replace(/ /g, '-')
-                })
-                contianer.querySelectorAll('pre').forEach(pre => pre.classList.add('z-code'))
-                import('./codeblock.js').then(({ initCodeBlock }) => {
-                    tocbot.destroy()
-                    initTocAndViewer(id)
-                    initCodeBlock()
-                })
-            }).catch(_ => alertify.error('marked load failed'))
-        }).catch(_ => alertify.error('CryptoJS load failed'))
-    } catch (_) {
-        alertify.error('解密失败')
-    }
+    loadJs('https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js').then(() => {
+        let res = undefined
+        try {
+            const _res = aesDecrypt(data, pwd)
+            res = _res
+        } catch (_) {
+            alertify.error('解密失败')
+            document.getElementById(`pwd-${hash}`).value = ''
+            return
+        }
+        if (!res) {
+            alertify.error('解密失败')
+            document.getElementById(`pwd-${hash}`).value = ''
+            return
+        }
+        alertify.success('解密成功')
+        document.getElementById(`secret-content-${hash}`).remove()
+        loadJs('https://cdnjs.cloudflare.com/ajax/libs/marked/14.1.3/marked.min.js').then(() => {
+            const id = `decrypted-content-${hash}`
+            const contianer = document.getElementById(id)
+            contianer.innerHTML += marked.parse(res)
+            contianer.querySelectorAll("h1, h2, h3, h4, h5").forEach(h => {
+                h.id = h.innerText.trim().replace(/ /g, '-')
+            })
+            contianer.querySelectorAll('pre').forEach(pre => pre.classList.add('z-code'))
+            import('./codeblock.js').then(({ initCodeBlock }) => {
+                tocbot.destroy()
+                initTocAndViewer(id)
+                initCodeBlock()
+            })
+        }).catch(_ => alertify.error('marked error:\n' + _))
+    }).catch(_ => alertify.error('CryptoJS error:\n' + _))
 }
+const loadedJsSet = new Set()
 const loadJs = (src) => {
+    if (loadedJsSet.has(src)) return Promise.resolve()
+    loadedJsSet.add(src)
     return new Promise((resolve, reject) => {
         const script = document.createElement('script')
         script.src = src
